6813a20e3c2465fc2832a240d2ba31872fb48c43
greglenane <gregory.lenane@mssm.edu>
2020-02-19 16:52:42 -0500

    Created function for creating boxplot of site vs entire system by DOW

--- UCC_Output.R
+++ UCC_Output.R
@@ -4,6 +4,7 @@ library(anytime)
 library(lubridate)
 library(ggplot2)
 library(reshape2)
+library(anytime)
 Location <- c("MS Express Care", "UC Union Square", "UC Broadway", "UC Cadman", "UC Columbus", "UC York")
 Days <- c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday")
 #Create DOW data frames for the entire repository
@@ -11,7 +12,7 @@ DOW <- DOW_Volume()
 #The most recent week of data
 DOW_Weekly <- DOW_Volume(Date=max(anytime(data$Arrival), na.rm = T)-604800)
 #Fiscal year to date
-DOW_FYTD <- DOW_Volume(Date = floor_date(Sys.Date(),"year"))
+DOW_FYTD <- DOW_Volume(Date = lubridate::floor_date(Sys.Date(),"year"))
 #Past 30 days
 DOW_30day <- DOW_Volume(Date=max(anytime(data$Arrival), na.rm = T)-2952000)
 
@@ -20,10 +21,14 @@ TOD <- TOD_Volume()
 #The most recent week of data
 TOD_Weekly <- TOD_Volume(Date=max(anytime(data$Arrival), na.rm = T)-604800)
 #Fiscal year to date
-TOD_FYTD <- TOD_Volume(Date = floor_date(Sys.Date(),"year"))
+TOD_FYTD <- TOD_Volume(Date = lubridate::floor_date(Sys.Date(),"year"))
 #Past 30 days
 TOD_30day <- TOD_Volume(Date=max(anytime(data$Arrival), na.rm = T)-2952000)
 
+#Compliance table for FYTD
+Compliance_FYTD <- compliance(Date = floor_date(Sys.Date(),"year"))
+#Compliance table for most recent week of data
+Compliance_Weekly <- compliance(Date = max(anytime(data$Arrival), na.rm = T)-604800)
 
 
 #grouped bar graph for all DOW data
@@ -44,9 +49,9 @@ bar_DOW <- function(DOW_table){
 
 #line graph for all TOD data
 line_TOD <- function(TOD_table){
-  threemelt <- melt(data = TOD_table[c(1:6,7),], id.vars="Location")
-  threemelt$variable <- as.numeric(threemelt$variable)
-  three <- ggplot(data=threemelt, aes(x=variable,y=value,group=Location,color=Location))+
+  dfmelt <- melt(data = TOD_table[c(1:6,7),], id.vars="Location")
+  dfmelt$variable <- as.numeric(dfmelt$variable)
+  two <- ggplot(data=dfmelt, aes(x=variable,y=value,group=Location,color=Location))+
     geom_line(size=1.5)+
     geom_point(size=2.75, colour = "black")+
     scale_x_continuous(breaks=c(6:23), limits=c(6,23))+
@@ -55,7 +60,7 @@ line_TOD <- function(TOD_table){
     ylab("Volume")+
     theme(plot.title=element_text(hjust=.5,size=20),
           axis.title = element_text(face="bold"))
-  return(three)
+  return(two)
 }
 #line_TOD(TOD_30day)
 #line_TOD(TOD_Weekly)
@@ -69,34 +74,58 @@ bar_DOW_site <- function(loc){
   df[3,2:8] <- df[3,2:8]/7
   dfmelt <-  melt(data=df, id.vars="Location")
   df$Location <- factor(df$Location, levels=c("FYTD","30 Day", "7 Day", "System Median"))
-  five <- ggplot(data = melt(data=df, id.vas="Location"),aes(fill=Location, x=variable,y=value))+
+  three <- ggplot(data = melt(data=df, id.vas="Location"),aes(fill=Location, x=variable,y=value))+
     geom_bar(position="dodge", stat="identity", colour = "Black") +
     ggtitle(paste0(loc," Volume by Day of Week"))+
     xlab("Day of Week")+
     ylab("Volume (Daily)")+
     theme(plot.title=element_text(hjust=.5,size=20),
           axis.title = element_text(face="bold"))
-  return(five)
+  return(three)
 }
 #bar_DOW_site("MS Express Care")
 
-box_DOW_site <- function(loc){
+box_DOW_site <- function(loc, Date = "1/1/2000"){
   data$Arrival <- anytime(data$Arrival)
   data <- cbind(data,as.Date(data$Arrival,format = "%m/%d/%Y"))
+  data <- data[data$`as.Date(data$Arrival, format = "%m/%d/%Y")`>Date,]
+  #all data for site of interest
   df1 <- data[data$Location == loc,]
   df1 <- df1[!is.na(df1$Location) & !is.na(df1$`Day of Week`),]
-  df2 <- data[data$Location != loc,]
+  #all data for entire system
+  df2 <- data
   df2 <- df2[!is.na(df2$Location) & !is.na(df2$`Day of Week`),]
   df2$Location <- "System"
+  #combine df1 and df2
   data1 <- rbind(df1,df2)
+  #create list of all unique dates
   daily <- unique(data1$`as.Date(data$Arrival, format = "%m/%d/%Y")`)
-  DailyVol <- as.data.frame(matrix(0,nrow=2,ncol = length(daily)))
+  #create table with number encounters by uniqe date for site and entire system
+  DailyVol <- as.data.frame(matrix(0,nrow=2,ncol = 1+length(daily)))
+  colnames(DailyVol) <- c("Location",as.character(daily))
+  DailyVol[,1] <- c(loc,"System")
+  #calculate number encounters by date for site
   for(i in 1:length(daily)){
-    DailyVol[1,i] <- nrow(data1[data1$Location == loc & data1$`as.Date(data$Arrival, format = "%m/%d/%Y")` == daily[i],])
+    DailyVol[1,i+1] <- nrow(data1[data1$Location == loc & data1$`as.Date(data$Arrival, format = "%m/%d/%Y")` == daily[i],])
   }
-  
-  
-  #datatest <- melt(data = data1, id = c(1,12), measure.vars = 10) 
-  
+  #Calculate number encounters by date for entire system
+  for(i in 1:length(daily)){
+    DailyVol[2,i+1] <- nrow(data1[data1$Location == "System" & data1$`as.Date(data$Arrival, format = "%m/%d/%Y")` == daily[i],])
+  }
+  #melt DailyVol so we have 2 rows for every date (1 for site and 1 for entire system)
+  dfmelt <- melt(data = DailyVol, id.vars = "Location")
+  dfmelt$variable <- weekdays(anytime(dfmelt$variable))
+  #Calculate system average for every system encounter value
+  dfmelt[dfmelt$Location == "System",][,3] <- (dfmelt[dfmelt$Location == "System",][,3])/length(Location)
+  dfmelt$variable <- factor(dfmelt$variable, levels = Days)
+  dfmelt$Location <- factor(dfmelt$Location, levels = c(loc,"System"))
+  four <- ggplot(data=dfmelt, aes(x=variable, y=value, fill=Location))+
+    geom_boxplot()+
+    ggtitle(paste0(loc," Encounters by Day of Week"))+
+    xlab("Day of Week")+
+    ylab("Volume (Encounters)")+
+    theme(plot.title=element_text(hjust=.5,size=20),
+          axis.title = element_text(face="bold"))
+  return(four)
 }
 
