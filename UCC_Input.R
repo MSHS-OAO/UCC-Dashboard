589634d818563d23e208b78d1b3fc174bb86fef0
greglenane <gregory.lenane@mssm.edu>
2020-04-22 17:06:45 -0400

    Adding sinai color functions

--- UCC_Input.R
+++ UCC_Input.R
@@ -139,3 +139,46 @@ compliance <- function(Date = "1/1/2000"){
   #Return Comp Table
   return(Comp_Table)
 }
+
+#Colors
+sinai_colors <- c(`Navy Blue` = "#221F72",`Light Blue` = "#00AEEF",`Hot Pink` = "#D80B8C",`Medium Grey` = "#B2B3B2",`Light Purple` = "#C7C6EF",`Light Grey` = "#DDDEDD",`Light Pink` = "#FCC9E9",`Dark Red` = "#C00000",`Green` = "#009242")
+sinai_cols <- function(...) {
+  cols <- c(...)
+    
+  if (is.null(cols))
+    return (sinai_colors)
+    
+  sinai_colors[cols]
+}
+sinai_palettes <- list(
+  `main`  = sinai_cols("Navy Blue", "Light Blue", "Hot Pink", "Medium Grey", "Light Purple", "Light Grey", "Light Pink"),
+  
+  `cool`  = sinai_cols("Navy Blue", "Light Blue", "Light Purple")
+)
+sinai_pal <- function(palette = "main", reverse = FALSE, ...) {
+  pal <- sinai_palettes[[palette]]
+  
+  if (reverse) pal <- rev(pal)
+  
+  colorRampPalette(pal, ...)
+}
+
+scale_color_sinai <- function(palette = "main", discrete = TRUE, reverse = FALSE, ...) {
+  pal <- sinai_pal(palette = palette, reverse = reverse)
+  
+  if (discrete) {
+    discrete_scale("colour", paste0("sinai_", palette), palette = pal, ...)
+  } else {
+    scale_color_gradientn(colours = pal(256), ...)
+  }
+}
+
+scale_fill_sinai <- function(palette = "main", discrete = TRUE, reverse = FALSE, ...) {
+  pal <- sinai_pal(palette = palette, reverse = reverse)
+  
+  if (discrete) {
+    discrete_scale("fill", paste0("sinai_", palette), palette = pal, ...)
+  } else {
+    scale_fill_gradientn(colours = pal(256), ...)
+  }
+}
