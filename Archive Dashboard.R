b4226c73705b9e8a5d9fd1953104bdbda8a1eeca
greglenane <gregory.lenane@mssm.edu>
2020-12-14 17:01:43 -0500

    directory update

--- Archive Dashboard.R
+++ Archive Dashboard.R
@@ -1,4 +1,4 @@
 library(htmltools)
 dir <- "C:/Users/lenang01/Documents/UCC-Dashboard"
-file.copy(paste0(dir,"/UCC_Dashboard.html"),paste0(dir,"/Dashboard Archive"))
-file.rename(paste0(dir,"/Dashboard Archive/UCC_Dashboard.html"),paste0(dir,"/Dashboard Archive/UCC_Dashboard_",Sys.Date(),".html"))
+file.copy("C:/Users/lenang01/Documents/UCC-Dashboard/UCC_Dashboard.html","C:/Users/lenang01/Documents/UCC-Dashboard/Dashboard Archive")
+file.rename("C:/Users/lenang01/Documents/UCC-Dashboard/Dashboard Archive/UCC_Dashboard.html"),paste0("C:/Users/lenang01/Documents/UCC-Dashboard/Dashboard Archive/UCC_Dashboard_",Sys.Date(),".html"))
