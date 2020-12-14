f798436f4ef93541d83c4fc0a00fd0d110e77ed2
greglenane <gregory.lenane@mssm.edu>
2020-12-14 17:04:33 -0500

    typo in directory

--- Archive Dashboard.R
+++ Archive Dashboard.R
@@ -1,4 +1,3 @@
 library(htmltools)
-dir <- "C:/Users/lenang01/Documents/UCC-Dashboard"
 file.copy("C:/Users/lenang01/Documents/UCC-Dashboard/UCC_Dashboard.html","C:/Users/lenang01/Documents/UCC-Dashboard/Dashboard Archive")
-file.rename("C:/Users/lenang01/Documents/UCC-Dashboard/Dashboard Archive/UCC_Dashboard.html"),paste0("C:/Users/lenang01/Documents/UCC-Dashboard/Dashboard Archive/UCC_Dashboard_",Sys.Date(),".html"))
+file.rename("C:/Users/lenang01/Documents/UCC-Dashboard/Dashboard Archive/UCC_Dashboard.html",paste0("C:/Users/lenang01/Documents/UCC-Dashboard/Dashboard Archive/UCC_Dashboard_",Sys.Date(),".html"))
